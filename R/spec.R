6812a72e19213ef0991173ced5381db46e35cf30
hadley <h.wickham@gmail.com>
2014-06-17 11:53:44 +0200

    Refine spec specification

--- R/spec.R
+++ R/spec.R
@@ -4,33 +4,36 @@
 #' not define yourself, but instead you will use \code{\link{guess_spec}} to
 #' guess from the contents of the file.
 #'
-#' @param col_parsers A list of vector parsers describing to how to
+#' @param parsers A list of vector parsers describing to how to
 #'   parser each individual column
 #' @param header If \code{TRUE}, the first row will be used to define column
 #'   names. If \code{FALSE}, columns will be given names X1-Xn.
 #' @param line_delim The delimiter between lines, usually \code{"\n"}
 #' @param field_delim The delimiter between fields. Either a character vector
-#'   or XXX meaning to split on any whitespace.
-#' @param na_string A character vector of strings which should be converted to
+#'   or \code{\link{whitespace}()} meaning to split on any whitespace.
+#' @param na_strings A character vector of strings which should be converted to
 #'   NA in R.
 #' @param comment_char Any lines starting with this character will be not be
 #'   processed.
 #' @param skip Skip this many lines before beginning parsing.
 #' @export
-spec <- function(col_parsers, header = TRUE, line_delim = "\n",
-                 field_delim = ",", comment_char = "#", na_string = "NA",
+spec <- function(parsers, header = TRUE, field_delim = ",",
+                 comment_char = "#", na_strings = "NA",
                  skip = 0) {
 
   structure(
     list(
-      col_parsers = col_parsers,
+      parsers = parsers,
       header = header,
-      line_delim = line_delim,
       field_delim = field_delim,
-      na_string = na_string,
       comment_char = comment_char,
+      na_strings = na_strings,
       skip = skip
     ),
     class = "spec"
   )
 }
+
+#' @export
+#' @rdname spec
+whitespace <- function() list("whitespace")
