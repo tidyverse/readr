694b6b119795be2fd270141ca383e0215f2c36d3
hadley <h.wickham@gmail.com>
2014-06-17 08:19:51 +0200

    Build a spec object

---
+++ R/spec.R
@@ -0,0 +1,32 @@
+#' Generate a spec object.
+#'
+#' A spec defines how a file will be read from disk. In most cases, you will
+#' not define yourself, but instead you will use \code{\link{guess_spec}} to
+#' guess from the contents of the file.
+#'
+#' @param col_parsers A list of vector parsers describing to how to
+#'   parser each individual column
+#' @param header If \code{TRUE}, the first row will be used to define column
+#'   names. If \code{FALSE}, columns will be given names X1-Xn.
+#' @param line_delim The delimiter between lines, usually \code{"\n"}
+#' @param field_delim The delimiter between fields. Either a character vector
+#'   or XXX meaning to split on any whitespace.
+#' @param comment_char Any lines starting with this character will be not be
+#'   processed.
+#' @param skip Skip this many lines before beginning parsing.
+#' @export
+spec <- function(col_parsers, header = TRUE, line_delim = "\n",
+                 field_delim = ",", comment_char = "#", skip = 0) {
+
+  structure(
+    list(
+      col_parsers = col_parsers,
+      header = header,
+      line_delim = line_delim,
+      field_delim = field_delim,
+      comment_char = comment_char,
+      skip = skip
+    ),
+    class = "spec"
+  )
+}
