d385d5e631c64149a235f33d804e42670f23487d
hadley <h.wickham@gmail.com>
2014-06-18 08:55:27 +0200

    Add basic tests

---
+++ inst/tests/test-parse-line-delimited.R
@@ -0,0 +1,15 @@
+context("parse_line_delimited")
+
+abc <- c("a", "b", "c")
+test_that("simple delimiters parsed as expected", {
+  expect_equal(parse_line_delimited("a,b,c"), abc)
+  expect_equal(parse_line_delimited("a|b|c", "|"), abc)
+})
+
+test_that("can escape delim if backslash_escape = TRUE", {
+  expect_equal(
+    parse_line_delimited("a,b\\,c", backslash_escape = TRUE),
+    c("a", "b,c")
+  )
+})
+
