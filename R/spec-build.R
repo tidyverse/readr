a9f971bf75c2b642b82f5cd86183d9c9bf7d3a05
hadley <h.wickham@gmail.com>
2014-06-18 13:49:06 +0200

    Sketch basic strategy for guessing parsers

--- R/spec-build.R
+++ R/spec-build.R
@@ -31,11 +31,17 @@ build_csv_spec <- function(file, parsers = NULL, header = TRUE, quote = '"',
 
 #' Guess which parsers are needed.
 #'
+#' This operation is relatively expensive, so it's only done on a small
+#' sample of the total data frame. This makes it prone to error if your
+#' data formats vary, but since the individual parsers are strict (once
+#' identified) you'll quickly find out about it, and you can specify the
+#' parsers exactly yourself.
+#'
 #' @param fields List of character vectors: each vector represents one
 #'   lines worth of fields.
 #' @export
 #' @keywords internal
-guess_parsers <- function(fields) {
+guess_parsers <- function(fields, na_strings = "NA", ignore_whitespace = TRUE) {
   nfields <- unique(vapply(fields, length, integer(1)))
 
   if (length(nfields) != 1) {
@@ -43,27 +49,29 @@ guess_parsers <- function(fields) {
       paste(nfields, collapse = ", "), collapse = ",")
   }
 
-  lapply(flip(fields), guess_parser)
+  lapply(flip(fields), guess_parser, na_strings = na_strings,
+    ignore_whitespace = ignore_whitespace)
 }
 
-guess_parser <- function(x) {
-  # Probably needs to be a C++ function:
-  # std::string guessParser(CharacterVector x)
-  #
-  # Fallback approach
-  #
-  # integer -> double -> character
-  # integer -> date time -> date -> character
-  #
-  # Other approach is to work from most general to most specific
+guess_parser <- function(x, na_strings = "NA", ignore_whitespace = FALSE) {
+  if (ignore_whitespace) {
+    x <- lapply(x, gsub, "^\\s+|\\s+$", "")
+  }
+  x <- lapply(x, setdiff, na_strings)
 
-  # [1-9][0-9]+ -> integer
-  # [+-]?(?=\d*[.eE])(?=\.?\d)\d*\.?\d*(?:[eE][+-]?\d+)? -> floating point
-  # T|F|TRUE|FALSE -> logical
-  # [0-9]{4}-[0-9]{2}-[0-9]{2} -> iso8601 date
-  # [0-9]{4}-[0-9]{2}-[0-9]{2}T... -> iso8601 date time
-  # -> character
-  "character"
+  coercible <- list(
+    logical = function(x) x %in% c("TRUE", "FALSE", "T", "F"),
+    integer = function(x) grepl(x, "^[0-9]+$"),
+    double  = function(x) !any(is.na(as.numeric(x))),
+    date    = function(x) grepl(x, "^[0-9]{4}-[0-9]{2}-[0-9]{2}$")
+  )
+
+  ok <- vapply(coercible, function(f) f(x), logical(1))
+  if (!any(ok)) {
+    return("character")
+  }
+
+  names(coercible)[ok]
 }
 
 
