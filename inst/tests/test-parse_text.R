9eec9e5559f13676fe91c68973ae2bd04b688eec
hadley <h.wickham@gmail.com>
2014-06-20 10:27:33 +0100

    Update parse_text tests to new syntax

--- inst/tests/test-parse_text.R
+++ inst/tests/test-parse_text.R
@@ -1,20 +1,20 @@
 context("parse_text")
 
 test_that( "parser skips quotes and spaces", {
-  expect_equal( parse_text( "'1.0'", "double" ), 1.0 )
-  expect_equal( parse_text( '"1.0"', "double" ), 1.0 )
-  expect_equal( parse_text( ' " 1.0"', "double" ), 1.0 )
-  
-  expect_equal( parse_text( "'1'", "int" ), 1L )
-  expect_equal( parse_text( '"1"', "int" ), 1L )
-  expect_equal( parse_text( ' " 1"', "int" ), 1L )
-  
+  expect_equal( parse_text( "'1.0'", double_parser()), 1.0 )
+  expect_equal( parse_text( '"1.0"', double_parser() ), 1.0 )
+  expect_equal( parse_text( ' " 1.0"', double_parser() ), 1.0 )
+
+  expect_equal( parse_text( "'1'", integer_parser() ), 1L )
+  expect_equal( parse_text( '"1"', integer_parser() ), 1L )
+  expect_equal( parse_text( ' " 1"', integer_parser() ), 1L )
+
 })
 
 test_that("parser escapes quotes", {
-  target <- "foo \"yada\" bar" 
-  expect_equal(parse_text('"foo ""yada"" bar"', "string" ), target)
-  expect_equal(parse_text('"foo \"yada\" bar"', "string" ), target)
-  expect_equal(parse_text('"foo "yada" bar"', "string" ), target)
+  target <- "foo \"yada\" bar"
+  expect_equal(parse_text('"foo ""yada"" bar"', character_parser() ), target)
+  expect_equal(parse_text('"foo \"yada\" bar"', character_parser() ), target)
+  expect_equal(parse_text('"foo "yada" bar"', character_parser() ), target)
 })
 
