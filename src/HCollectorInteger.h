68aecf8b3e76aac83d1ae1203aebaccaaef5aa04
hadley <h.wickham@gmail.com>
2015-03-03 11:33:59 -0600

    Sketch integer parsing

---
+++ src/HCollectorInteger.h
@@ -0,0 +1,57 @@
+#ifndef FASTREAD_COLLECTORINTEGER_H
+#define FASTREAD_COLLECTORINTEGER_H
+
+#include "HCollector.h"
+#include <Rcpp.h>
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+
+class CollectorInteger : Collector {
+  Rcpp::IntegerVector column_;
+
+public:
+
+  CollectorInteger(): column_(0) {
+  }
+
+  void resize(int n) {
+    column_ = Rf_lengthgets(column_, n);
+  }
+
+  void setValue(int i, const Token& t) {
+    column_[i] = parse(t);
+  }
+
+  int parse(const Token& t) {
+    int res = 0;
+
+    switch(t.type()) {
+    case TOKEN_POINTER:
+      boost::spirit::qi::parse(t.begin(), t.end(),
+        boost::spirit::qi::int_, res);
+      break;
+    case TOKEN_INLINE:
+      boost::spirit::qi::parse(t.text().begin(), t.text().end(),
+        boost::spirit::qi::int_, res);
+      break;
+    case TOKEN_MISSING:
+    case TOKEN_EMPTY:
+      res = NA_REAL;
+      break;
+    case TOKEN_EOL:
+    case TOKEN_EOF:
+      Rcpp::stop("Invalid token");
+    }
+
+    return res;
+  }
+
+  SEXP vector() {
+    return column_;
+  }
+
+};
+
+#endif
