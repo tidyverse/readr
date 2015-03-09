fa5030ab40560b555ffba1e10117bed3964a72a9
hadley <h.wickham@gmail.com>
2015-03-09 13:26:55 -0500

    Pass on n_max to read_tokens

--- R/read_csv.R
+++ R/read_csv.R
@@ -4,6 +4,7 @@
 #' @inheritParams tokenizer_csv
 #' @inheritParams col_names_standardise
 #' @inheritParams col_types_standardise
+#' @param n_max Maximum number of records to read.
 #' @export
 #' @examples
 #' read_csv(text = "x,y\n1,2\n3,4")
@@ -11,7 +12,7 @@
 #' # Override default col types
 #' read_csv(text = "x,y\n1,2\n3,4", col_types = "dd")
 read_csv <- function(path, text, col_names = TRUE, col_types = NULL, na = "NA",
-                     skip = 0) {
+                     skip = 0, n_max = -1) {
   source <- new_source(path, text, skip = skip)
   tokenizer <- tokenizer_csv(na = na)
 
@@ -21,7 +22,7 @@ read_csv <- function(path, text, col_names = TRUE, col_types = NULL, na = "NA",
 
   source <- new_source(path, text, skip = skip)
   col_types <- col_types_standardise(col_types, col_names, types(source, tokenizer))
-  read_tokens(source, tokenizer, col_types, col_names)
+  read_tokens(source, tokenizer, col_types, col_names, n_max = n_max)
 }
 
 # The header is the first row, parsed into fields
