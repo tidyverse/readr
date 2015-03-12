2bcdcd1e7414119f525f1d1c4cd9e42383473796
hadley <h.wickham@gmail.com>
2015-03-12 06:19:42 -0500

    Coerce non-atomic vectors in write_csv

--- R/write.R
+++ R/write.R
@@ -1,7 +1,8 @@
 #' Save a data frame to a csv file.
 #'
 #' This is about twice as fast as \code{\link{write.csv}}, and never
-#' writes row names.
+#' writes row names. Non-atomic vectors are coerced to character vectors
+#' with \code{as.character}.
 #'
 #' @param x A data frame to write to disk
 #' @param path Path to write to.
@@ -11,6 +12,9 @@
 #' @export
 write_csv <- function(x, path, append = FALSE, col_names = !append) {
   stopifnot(is.data.frame(x))
+
+  is_object <- vapply(x, is.object, logical(1))
+  x[is_object] <- lapply(x[is_object], as.character)
 
   path <- normalizePath(path, mustWork = FALSE)
   stream_csv(x, path, col_names = col_names, append = append)
