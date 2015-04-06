b42e8d5df64178aef4b88ecada163bd7fe3d2d94
hadley <h.wickham@gmail.com>
2015-04-06 16:42:10 -0500

    Minor improvements to locale

--- src/DateTimeLocale.h
+++ src/DateTimeLocale.h
@@ -3,8 +3,9 @@
 
 #include <ctime>
 #include "Iconv.h"
+#include <boost/noncopyable.hpp>
 
-class DateTimeLocale {
+class DateTimeLocale : boost::noncopyable {
   std::vector<std::string> month_, monthAbbrev_, day_, dayAbbrev_, period_;
   Iconv encoder_;
   char buff_[100];
@@ -31,24 +32,24 @@ public:
     for(int i = 0; i < 12; i++) {
       tm.tm_mon = i;
 
-      month_.push_back(encoder_.convert(formatDate(tm, "%B")));
-      monthAbbrev_.push_back(encoder_.convert(formatDate(tm, "%b")));
+      month_.push_back(formatDate(tm, "%B"));
+      monthAbbrev_.push_back(formatDate(tm, "%b"));
     }
     tm.tm_mon = 0;
 
     for(int i = 0; i < 7; i++) {
-      tm.tm_mday = tm.tm_yday = i+1; /* 2000-01-02 was a Sunday */
+      tm.tm_mday = tm.tm_yday = i + 1; /* 2000-01-02 was a Sunday */
       tm.tm_wday = i;
 
-      day_.push_back(encoder_.convert(formatDate(tm, "%A")));
-      dayAbbrev_.push_back(encoder_.convert(formatDate(tm, "%a")));
+      day_.push_back(formatDate(tm, "%A"));
+      dayAbbrev_.push_back(formatDate(tm, "%a"));
     }
 
     tm.tm_hour = 1;
-    period_.push_back(encoder_.convert(formatDate(tm, "%p")));
+    period_.push_back(formatDate(tm, "%p"));
 
     tm.tm_hour = 13;
-    period_.push_back(encoder_.convert(formatDate(tm, "%p")));
+    period_.push_back(formatDate(tm, "%p"));
   }
 
   const std::vector<std::string>& month() const {
@@ -70,7 +71,7 @@ public:
 private:
   inline std::string formatDate(struct tm& tm, const char* format) {
     size_t res = strftime(buff_, 100, format, &tm);
-    return std::string((res == 0) ? "" : buff_);
+    return std::string((res == 0) ? "" : encoder_.convert(buff_));
   }
 
 };
