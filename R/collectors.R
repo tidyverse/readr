954c6ce8d42700efd1979b6d39120a25f06db1c7
hadley <h.wickham@gmail.com>
2015-04-06 09:55:18 -0500

    Add parser wrappers for all collectors

--- R/collectors.R
+++ R/collectors.R
@@ -11,9 +11,20 @@ collector_find <- function(name) {
   get(paste0("col_", name), envir = asNamespace("readr"))()
 }
 
-#' Column parsing specifications.
+#' Parse character vectors into typed columns.
+#'
+#' Use \code{parse_} if you have a character vector you want to parse. Use
+#' \code{col_} in conjunction with a \code{read_} function to parse the
+#' values as they're read in.
 #'
 #' @name collector
+#' @seealso \code{\link{parse_date}}
+#' @examples
+#' parse_integer(c("1", "2", "3"))
+#' parse_double(c("1", "2", "3.123"))
+#' parse_euro_double(c("1", "2", "3,123"))
+#' parse_factor(c("a", "b"), letters)
+#' parse_numeric("$1,123,456.000")
 NULL
 
 #' @rdname collector
@@ -24,8 +35,20 @@ col_character <- function() {
 
 #' @rdname collector
 #' @export
+parse_character <- function(x) {
+  parse_vector(x, col_character())
+}
+
+#' @rdname collector
+#' @export
 col_integer <- function() {
   collector("integer")
+}
+
+#' @rdname collector
+#' @export
+parse_integer <- function(x) {
+  parse_vector(x, col_integer())
 }
 
 #' @rdname collector
@@ -36,9 +59,22 @@ col_double <- function() {
 
 #' @rdname collector
 #' @export
+parse_double <- function(x) {
+  parse_vector(x, col_double())
+}
+
+#' @rdname collector
+#' @export
 col_euro_double <- function() {
   collector("euro_double")
 }
+
+#' @rdname collector
+#' @export
+parse_euro_double <- function(x) {
+  parse_vector(x, col_euro_double())
+}
+
 
 #' @rdname collector
 #' @export
@@ -48,16 +84,20 @@ col_numeric <- function() {
 
 #' @rdname collector
 #' @export
+parse_numeric <- function(x) {
+  parse_vector(x, col_numeric())
+}
+
+#' @rdname collector
+#' @export
 col_logical <- function() {
   collector("logical")
 }
 
-#' @param format Format string passed on to the \code{\link{parse_date}}.
-#' @param tz Default time zone to use for input and output (format).
 #' @rdname collector
 #' @export
-col_datetime <- function(format = "", tz = "UTC") {
-  collector("datetime", format = format, tz = tz)
+parse_logical <- function(x) {
+  parse_vector(x, col_logical())
 }
 
 #' @param levels Character vector providing set of allowed levels.
@@ -68,9 +108,25 @@ col_factor <- function(levels, ordered = FALSE) {
   collector("factor", levels = levels, ordered = ordered)
 }
 
+#' @rdname collector
+#' @export
+parse_factor <- function(x, levels, ordered = FALSE) {
+  parse_vector(x, col_factor(levels, ordered))
+}
 
 #' @rdname collector
 #' @export
 col_skip <- function() {
   collector("skip")
 }
+
+# More complex ------------------------------------------------------------
+
+#' @param format Format string passed on to the \code{\link{parse_date}}.
+#' @param tz Default time zone to use for input and output (format).
+#' @rdname parse_date
+#' @export
+col_datetime <- function(format = "", tz = "UTC") {
+  collector("datetime", format = format, tz = tz)
+}
+
