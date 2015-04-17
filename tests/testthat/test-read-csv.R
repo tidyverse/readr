0e4cc5ec7019e966cef21b744c232aed1c342a06
hadley <h.wickham@gmail.com>
2015-04-17 06:36:00 -0500

    WS

--- tests/testthat/test-read-csv.R
+++ tests/testthat/test-read-csv.R
@@ -1,27 +1,28 @@
 context("read_csv parameter testing")
 
-test_that("read_csv col imputation, col_name detection and NA detection works",{
+test_that("read_csv col imputation, col_name detection and NA detection works", {
   test_data <- read_csv("basic-df.csv", col_types = NULL, col_names = TRUE)
-  expect_equal(unname(unlist(lapply(test_data,class))), c("logical","integer","numeric","character"))
-  expect_equal(names(test_data), c("a","b","c","d"))
+  expect_equal(unname(unlist(lapply(test_data, class))),
+    c("logical", "integer", "numeric", "character"))
+  expect_equal(names(test_data), c("a", "b", "c", "d"))
   expect_equal(sum(is.na(test_data$d)), 1)
 })
 
-test_that("read_csv's 'NA' option genuinely changes the NA values",{
+test_that("read_csv's 'NA' option genuinely changes the NA values", {
   expect_equal(read_csv("a\nz", na = "z")$a, NA_character_)
 })
 
-test_that("read_csv's 'skip' option allows for skipping'",{
+test_that("read_csv's 'skip' option allows for skipping'", {
   test_data <- read_csv("basic-df.csv", skip = 1)
   expect_equal(nrow(test_data), 9)
 })
 
-test_that("read_csv's 'skip' option allows for skipping when no header row is present'",{
+test_that("read_csv's 'skip' option allows for skipping when no header row is present'", {
   test_data <- read_csv("basic-df.csv", skip = 1, col_names = FALSE)
   expect_equal(nrow(test_data), 10)
 })
 
-test_that("read_csv's 'n_max' allows for a maximum number of records and does not corrupt any",{
+test_that("read_csv's 'n_max' allows for a maximum number of records and does not corrupt any", {
   test_data <- read_csv("basic-df.csv", n_max = 7)
   expect_equal(nrow(test_data), 7)
   expect_equal(sum(is.na(test_data)), 0)
