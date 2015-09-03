6b11595c443fd3799888126acadc38b432fb9f9d
hadley <h.wickham@gmail.com>
2015-09-03 14:28:17 -0500

    Delete sentence fragment. Closes #184

--- R/read_lines.R
+++ R/read_lines.R
@@ -2,7 +2,7 @@
 #'
 #' @inheritParams datasource
 #' @param n_max Number of lines to read. If \code{n} is -1, all lines in
-#'   file will be read. For large files, this may
+#'   file will be read.
 #' @return A character vector with one element for each line.
 #' @export
 #' @examples
