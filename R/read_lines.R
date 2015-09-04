0f5925552469db277ad6bd6e68edf38a3fa9e6b2
hadley <h.wickham@gmail.com>
2015-09-04 16:59:28 -0500

    progress argument for read_lines

--- R/read_lines.R
+++ R/read_lines.R
@@ -1,6 +1,7 @@
 #' Read lines from a file or string.
 #'
 #' @inheritParams datasource
+#' @inheritParams read_delim
 #' @param n_max Number of lines to read. If \code{n} is -1, all lines in
 #'   file will be read.
 #' @return A character vector with one element for each line.
@@ -9,7 +10,7 @@
 #' read_lines(system.file("extdata/mtcars.csv", package = "readr"))
 #' read_lines("1\n\n2")
 #' read_lines("\n")
-read_lines <- function(file, n_max = -1L) {
+read_lines <- function(file, n_max = -1L, progress = interactive()) {
   ds <- datasource(file)
-  read_lines_(ds, n_max = n_max)
+  read_lines_(ds, n_max = n_max, progress = progress)
 }
