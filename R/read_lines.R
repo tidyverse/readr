7d7f5601f23d236fcd200fa3c5a7d3a2e8b391c1
Nicolas Coutin <nicolas.coutin@gmail.com>
2015-09-06 00:37:22 -0700

    expose skip to read_lines()

--- R/read_lines.R
+++ R/read_lines.R
@@ -10,7 +10,7 @@
 #' read_lines(system.file("extdata/mtcars.csv", package = "readr"))
 #' read_lines("1\n\n2")
 #' read_lines("\n")
-read_lines <- function(file, n_max = -1L, progress = interactive()) {
-  ds <- datasource(file)
+read_lines <- function(file, skip = 0, n_max = -1L, progress = interactive()) {
+  ds <- datasource(file, skip = skip)
   read_lines_(ds, n_max = n_max, progress = progress)
 }
