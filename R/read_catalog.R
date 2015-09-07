8e06a233d371c2545d64081f07361b8f9e1b58a2
Nicolas Coutin <nicolas.coutin@gmail.com>
2015-09-07 16:45:07 -0700

    simpler to use pryr::make_call. actually make examples work.

--- R/read_catalog.R
+++ R/read_catalog.R
@@ -8,29 +8,27 @@
 #' @param bind boolean. Should \code{dplyr::bind_rows()} be called on output.
 #'  Defaults to FALSE.
 #' @examples
-#'  \dontrun{
 #'  # first we make a catalog
 #'  files <- c(system.file("extdata/mtcars.csv", package = "readr"),
-#'  system.file("extdata/mtcars.csv.zip", package = "readr"),
-#'  system.file("extdata/mtcars.csv.bz2", package = "readr"))
+#'            system.file("extdata/mtcars.csv.zip", package = "readr"),
+#'            system.file("extdata/mtcars.csv.bz2", package = "readr"))
 #'  catalog <- dplyr::data_frame(file = files,
-#'                               read_fun = readr::read_csv,
-#'                               dots = c(list(skip = 1), list(n_max = 2), list()))
+#'                              read_fun = "read_csv",
+#'                              dots = list(list(skip = 1),
+#'                                          list(n_max = 1),
+#'                                         list(col_names = F)))
 #'  read_catalog(catalog)
-#'  }
+#'  read_catalog(catalog, bind = TRUE)
 #' @export
 read_catalog <- function(catalog, bind = FALSE) {
-  # fill in specified params for each fun and return the partialled fun
-  funs <- purrr::map2(catalog$read_fun, catalog$dots, compose_function)
-  # read each file in catalog with its speficied fun
-  l <- purrr::map2(catalog$file, funs)
-  # bind the output ?
-  if (bind)
-    l <- dplyr::bind_rows(l)
+  df <- catalog
+  calls <- purrr::map3(df$file, df$read_fun, df$dots, compose_call)
+  l <- purrr::map(calls, eval)
+  if (bind) l <- dplyr::bind_rows(l)
   l
 }
 
-compose_function <- function(fun, dots) {
-  f <- match.fun(fun)
-  purrr::partial(f, dots)
+compose_call <- function(file, fun, dots) {
+  dots <- c(list(file = file), dots)
+  pryr::make_call(fun, .args = dots)
 }
