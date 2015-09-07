82a44a31f20ae7e22e86f180aaf06bfc429ae6c3
hadley <h.wickham@gmail.com>
2015-09-07 07:52:27 -0500

    Tests for parsing logicals

---
+++ tests/testthat/test-parsing-logical.R
@@ -0,0 +1,15 @@
+context("Parsing, logical")
+
+test_that("TRUE and FALSE parsed", {
+  expect_equal(parse_logical(c("TRUE", "FALSE")), c(TRUE, FALSE))
+})
+
+test_that("T and F parsed", {
+  expect_equal(parse_logical(c("T", "F")), c(TRUE, FALSE))
+})
+
+test_that("other values generate warnings", {
+  out <- parse_logical(c("A", "AB", "ABCD", "ABCDE", "NA"))
+  expect_equivalent(out, rep(NA, 5))
+  expect_equal(n_problems(out), 4)
+})
