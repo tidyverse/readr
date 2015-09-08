d8f8a4f48e4d42f08ae20e04a88c5a17ae6c5054
Nicolas Coutin <nicolas.coutin@gmail.com>
2015-09-07 22:40:58 -0700

    expose .id param.

--- R/read_catalog.R
+++ R/read_catalog.R
@@ -9,16 +9,17 @@
 #' function to use to read the input, and \code{in_args} a named list of the
 #' arguments to be used when reading the input.
 #'
-#' @param inca (df) A input catalog (inca).
+#' @param inca (df). A input catalog (inca).
 #' @param bind (lgl). Should \code{dplyr::bind_rows()} be called on output?
+#' @param .id (chr). Name of the id column to use if bind is set to TRUE.
 #'  Defaults to FALSE.
 #' @examples
 #'  # see the vignette.
 #' @export
-read_catalog <- function(inca, bind = FALSE) {
+read_catalog <- function(inca, bind = FALSE, .id = "id") {
   calls <- compose_calls(inca$in_name, inca$in_fun, inca$in_args)
   l <- purrr::map(calls, eval)
-  if (bind) l <- dplyr::bind_rows(l)
+  if (bind) l <- dplyr::bind_rows(l, .id = .id)
   l
 }
 
