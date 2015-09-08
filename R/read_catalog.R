40cd19a576cfe3bad21240c21f9f95a06e28ceff
Nicolas Coutin <nicolas.coutin@gmail.com>
2015-09-07 18:57:15 -0700

    simplify and re-format. Add a vignette to walk through my thought-process.

--- R/read_catalog.R
+++ R/read_catalog.R
@@ -1,34 +1,28 @@
-#' Read multiple files from a file catalog
+#' Read multiple files from an input catalog
 #'
-#' A file catalog is a \code{data.frame} object (df) where each observation is a
-#' specification of how to read in a particular file. The minimum variables are
-#' \code{file}, \code{read_fun} and \code{dots}.
+#' An input catalog (inca for short?) is a table that catalogs the inputs of a
+#' project. It is a `data.frame` object where each observation/record/row
+#' represents an input: something to be inputed into R relevant to your project.
 #'
-#' @param catalog df. A file catalog.
-#' @param bind boolean. Should \code{dplyr::bind_rows()} be called on output.
+#' Defining variables are: \code{in_name} a character string of the human-friendly
+#' name of the input, \code{in_fun} a character string of the name of the
+#' function to use to read the input, and \code{in_args} a named list of the
+#' arguments to be used when reading the input.
+#'
+#' @param inca (df) A input catalog (inca).
+#' @param bind (lgl). Should \code{dplyr::bind_rows()} be called on output?
 #'  Defaults to FALSE.
 #' @examples
-#'  # first we make a catalog
-#'  files <- c(system.file("extdata/mtcars.csv", package = "readr"),
-#'            system.file("extdata/mtcars.csv.zip", package = "readr"),
-#'            system.file("extdata/mtcars.csv.bz2", package = "readr"))
-#'  catalog <- dplyr::data_frame(file = files,
-#'                              read_fun = "read_csv",
-#'                              dots = list(list(skip = 1),
-#'                                          list(n_max = 1),
-#'                                         list(col_names = FALSE)))
-#'  read_catalog(catalog)
-#'  read_catalog(catalog, bind = TRUE)
+#'  # see the vignette.
 #' @export
-read_catalog <- function(catalog, bind = FALSE) {
-  df <- catalog
-  calls <- purrr::map3(df$file, df$read_fun, df$dots, compose_call)
+read_catalog <- function(inca, bind = FALSE) {
+  calls <- compose_calls(inca$in_name, inca$in_fun, inca$in_args)
   l <- purrr::map(calls, eval)
   if (bind) l <- dplyr::bind_rows(l)
   l
 }
 
-compose_call <- function(file, fun, dots) {
-  dots <- c(list(file = file), dots)
-  pryr::make_call(fun, .args = dots)
+compose_calls <- function(name, f, args) {
+  l <- purrr::map2(f, args, pryr::make_call)
+  setNames(l, name)
 }
