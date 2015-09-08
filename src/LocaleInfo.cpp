1816647a2013e5ab096c70bec986007b80250975
hadley <h.wickham@gmail.com>
2015-09-08 16:01:34 -0500

    C++ interface for locale and wrapper in between

---
+++ src/LocaleInfo.cpp
@@ -0,0 +1,23 @@
+#include <Rcpp.h>
+#include "LocaleInfo.h"
+
+using namespace Rcpp;
+
+LocaleInfo::LocaleInfo(List x) {
+  std::string klass = x.attr("class");
+  if (klass != "locale")
+    stop("Invalid input: must be of class locale");
+
+  List date_names = as<List>(x["date_names"]);
+  mon_ =   as<std::vector<std::string> >(date_names["mon"]);
+  monAb_ = as<std::vector<std::string> >(date_names["mon_ab"]);
+  day_ =   as<std::vector<std::string> >(date_names["day"]);
+  dayAb_ = as<std::vector<std::string> >(date_names["day_ab"]);
+  amPm_ = as<std::vector<std::string> >(date_names["am_pm"]);
+
+  decimalMark_ = as<char>(x["decimal_mark"]);
+  groupingMark_ = as<char>(x["grouping_mark"]);
+
+  tz_ = as<std::string>(x["tz"]);
+  encoding_ = as<std::string>(x["encoding"]);
+}
