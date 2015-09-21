0a853368d4bd31c37bd7fb9b4712f6938ec4373e
hadley <h.wickham@gmail.com>
2015-09-21 10:33:56 -0500

    Add sample data creation

--- tests/testthat/test-read-file.R
+++ tests/testthat/test-read-file.R
@@ -1,5 +1,12 @@
 context("read_file")
 
+# df <- dplyr::data_frame(français = "élève")
+# write.csv(df,
+#   "tests/testthat/enc-iso-8859-1.txt",
+#   fileEncoding = "ISO-8859-1",
+#   row.names = FALSE,
+#   quote = FALSE)
+
 test_that("read_file respects encoding", {
   x <- read_file("enc-iso-8859-1.txt", locale(encoding = "ISO-8859-1"))
   expect_equal(substr(x, 5, 5), "ç")
