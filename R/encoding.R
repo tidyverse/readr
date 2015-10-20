af1a4d54b271dff7467b7cfd87f9763de073263d
hadley <h.wickham@gmail.com>
2015-10-20 07:48:46 -0500

    Escape unicode for solaris

--- R/encoding.R
+++ R/encoding.R
@@ -10,7 +10,7 @@
 #' @export
 #' @examples
 #' guess_encoding(system.file("extdata/mtcars.csv", package = "readr"))
-#' guess_encoding("a\nµµ")
+#' guess_encoding("a\n\u00b5\u00b5")
 guess_encoding <- function(file, n_max = 1e4, threshold = 0.20) {
   if (!requireNamespace("stringi", quietly = TRUE)) {
     stop("stringi package required for encoding operations", call. = FALSE)
