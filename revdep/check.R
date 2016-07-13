6df1fd0b68459de3007e16620544437e267a42ed
hadley <h.wickham@gmail.com>
2016-07-13 14:15:45 -0400

    Modernise revdep check script

--- revdep/check.R
+++ revdep/check.R
@@ -2,5 +2,6 @@ library("devtools")
 
 # Don't forget to email javier@rstudio.com and ask him to check the IDE
 
-res <- revdep_check()
-revdep_check_save_summary(res)
+revdep_check()
+revdep_check_save_summary()
+revdep_check_print_problems()
