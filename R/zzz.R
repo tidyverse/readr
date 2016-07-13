b0237914568503f014f52d7036f7e36e76526c0f
hadley <h.wickham@gmail.com>
2016-07-13 14:15:14 -0400

    Add reminders to check with IDE team
    Fixes #464

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+release_questions <- function() {
+  c(
+    "Have checked with the IDE team?"
+  )
+}
--- revdep/check.R
+++ revdep/check.R
@@ -1,4 +1,6 @@
 library("devtools")
 
+# Don't forget to email javier@rstudio.com and ask him to check the IDE
+
 res <- revdep_check()
 revdep_check_save_summary(res)
