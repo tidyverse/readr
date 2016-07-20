e5a3445541f5f90de0ad94ea0affe8adf00f6b4f
Jim Hester <james.f.hester@gmail.com>
2016-07-20 10:55:40 -0400

    Revert changes to Source.cpp

--- src/Source.cpp
+++ src/Source.cpp
@@ -7,24 +7,20 @@ using namespace Rcpp;
 #include "SourceRaw.h"
 
 SourcePtr Source::create(List spec) {
-  return SourcePtr(Source::createDumbPtr(spec));
-}
-
-Source* Source::createDumbPtr(List spec) {
   std::string subclass(as<CharacterVector>(spec.attr("class"))[0]);
 
   int skip = as<int>(spec["skip"]);
   std::string comment = as<std::string>(spec["comment"]);
 
   if (subclass == "source_raw") {
-    return new SourceRaw(as<RawVector>(spec[0]), skip, comment);
+    return SourcePtr(new SourceRaw(as<RawVector>(spec[0]), skip, comment));
   } else if (subclass == "source_string") {
-    return new SourceString(as<CharacterVector>(spec[0]), skip, comment);
+    return SourcePtr(new SourceString(as<CharacterVector>(spec[0]), skip, comment));
   } else if (subclass == "source_file") {
     std::string path(as<CharacterVector>(spec[0])[0]);
-    return new SourceFile(path, skip, comment);
+    return SourcePtr(new SourceFile(path, skip, comment));
   }
 
   Rcpp::stop("Unknown source type");
-  return NULL;
+  return SourcePtr();
 }
