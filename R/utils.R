5ca7b3eb6f0b9b8daff687172024e555ab62d1fe
Jim Hester <james.f.hester@gmail.com>
2016-07-21 10:10:00 -0400

    Handle printing of non-syntactic names in col specs (#488)
    * Handle printing of non-syntactic names in col specs
    
    * Escape internal backticks in names

--- R/utils.R
+++ R/utils.R
@@ -7,3 +7,5 @@ isFALSE <- function(x) identical(x, FALSE)
 is.connection <- function(x) inherits(x, "connection")
 
 `%||%` <- function(a, b) if (is.null(a)) b else a
+
+is_syntactic <- function(x) make.names(x) == x
