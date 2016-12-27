c72b4b5ed269a1633563003b8245da36165153d2
Abraham Neuwirth <Yeedle@users.noreply.github.com>
2016-12-27 16:51:14 -0500

    added documentation

--- R/read_table.R
+++ R/read_table.R
@@ -14,6 +14,8 @@
 #' @inheritParams datasource
 #' @inheritParams tokenizer_fwf
 #' @inheritParams read_delim
+#' @param n The number of lines the tokenizer will read to determine the structure
+#'   of the file, 100 by default.
 #' @export
 #' @examples
 #' # One corner from http://www.masseyratings.com/cf/compare.htm
