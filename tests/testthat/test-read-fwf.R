15899a6d2c7b883ec8cb0c26c0547798df2fb631
Jim Hester <james.f.hester@gmail.com>
2017-02-10 16:00:49 -0500

    Pass progress = FALSE to prevent column printing

--- tests/testthat/test-read-fwf.R
+++ tests/testthat/test-read-fwf.R
@@ -126,3 +126,23 @@ test_that("error on empty spec (#511, #519)", {
   pos = fwf_positions(start = numeric(0), end = numeric(0))
   expect_error(read_fwf(txt, pos), "Zero-length.*specifications not supported")
 })
+
+# read_table -------------------------------------------------------------------
+
+test_that("read_table silently reads ragged last column", {
+  x <- read_table("foo bar\n1   2\n3   4\n5   6\n", progress = FALSE)
+  expect_equal(x$foo, c(1, 3, 5))
+})
+
+test_that("read_table skips all comment lines", {
+  x <- read_table("foo bar\n1   2\n3   4\n5   6\n", progress = FALSE)
+
+  y <- read_table("#comment1\n#comment2\nfoo bar\n1   2\n3   4\n5   6\n", progress = FALSE, comment = "#")
+
+  expect_equal(x, y)
+})
+
+test_that("read_table can read from a pipe (552)", {
+  x <- read_table(pipe("echo a b c && echo 1 2 3 && echo 4 5 6"), progress = FALSE)
+  expect_equal(x$a, c(1, 4))
+})
