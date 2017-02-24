e94fb47a97044af622834808b684a02067cc508c
Jim Hester <james.f.hester@gmail.com>
2017-02-24 11:35:35 -0500

    Reverse dependency results

--- revdep/check.R
+++ revdep/check.R
@@ -2,8 +2,8 @@ library("devtools")
 
 # Don't forget to email javier@rstudio.com and ask him to check the IDE
 
-revdep_check()
+revdep_check_resume()
 revdep_check_save_summary()
 revdep_check_print_problems()
 
-revdep_email(date = "July 29", draft = FALSE)
+#revdep_email(date = "July 29", draft = FALSE)
