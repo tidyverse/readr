cc020664e919afeab6b9e50c76c45d36a5f29c23
jennybc <jenny@stat.ubc.ca>
2017-04-05 11:30:57 -0700

    List files with readr_example() and remove internal keyword

--- R/example.R
+++ R/example.R
@@ -3,11 +3,15 @@
 #' readr comes bundled with a number of sample files in its `inst/extdata`
 #' directory. This function make them easy to access
 #'
-#' @param path Name of file
+#' @param path Name of file. If `NULL`, the example files will be listed.
 #' @export
-#' @keywords internal
 #' @examples
+#' readr_example()
 #' readr_example("challenge.csv")
-readr_example <- function(path) {
-  system.file("extdata", path, package = "readr", mustWork = TRUE)
+readr_example <- function(path = NULL) {
+  if (is.null(path)) {
+    dir(system.file("extdata", package = "readr"))
+  } else {
+    system.file("extdata", path, package = "readr", mustWork = TRUE)
+  }
 }
