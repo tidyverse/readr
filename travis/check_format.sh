746391759af0434f82ddc9052d9d76786c02956e
Jim Hester <james.f.hester@gmail.com>
2017-04-19 17:30:52 -0400

    Try moving logic to check_format.sh

--- travis/check_format.sh
+++ travis/check_format.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-set -e
-
 shopt -s extglob
-clang-format -i src/!(@(RcppExports|localtime|grisu3)).@(c|h|cpp) && git diff-files -U --exit-code
+
+clang_format=$(command -v clang-format)
+if $clang_format --version | grep -q '3.4'; then
+  clang_format=$(command -v clang-format-3.9)
+fi
+if [ -z "$clang_format" ]; then
+  echo "Could not find recent version of clang-format"
+  exit 1
+fi
+"$clang_format" -i src/!(@(RcppExports|localtime|grisu3)).@(c|h|cpp) && git diff-files -U --exit-code
