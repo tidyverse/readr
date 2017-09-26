8c83b5c00aa21b13b7cbb34f325051ca65fb0664
olgamie <olga.mierzwa@gmail.com>
2017-09-26 12:43:29 +0200

    add write_excel_csv2 function

--- R/write.R
+++ R/write.R
@@ -8,7 +8,7 @@
 #' Factors are coerced to character. Doubles are formatted using the grisu3
 #' algorithm. POSIXct's are formatted as ISO8601.
 #'
-#' All columns are encoded as UTF-8. `write_excel_csv()` also includes a
+#' All columns are encoded as UTF-8. `write_excel_csv()` and `write_excel_csv2()` also include a
 #' \href{https://en.wikipedia.org/wiki/Byte_order_mark}{UTF-8 Byte order mark}
 #' which indicates to Excel the csv is UTF-8 encoded.
 #'
@@ -20,8 +20,8 @@
 #'   will append to existing file. In both cases, if file does not exist a new
 #'   file is created.
 #' @param col_names Write columns names at the top of the file?
-#' @param delim Delimiter used to separate values. Defaults to `" "`. Must be
-#'   a single character.
+#' @param delim Delimiter used to separate values. Defaults to `" "`. `write_excel_csv()` has `","` as default and
+#' `write_excel_csv2` has `";"` as default. Must be a single character.
 #' @param na String used for missing values. Defaults to NA. Missing values
 #'   will never be quoted; strings with the same value as `na` will
 #'   always be quoted.
@@ -72,14 +72,25 @@ write_csv <- function(x, path, na = "NA", append = FALSE, col_names = !append) {
 
 #' @rdname write_delim
 #' @export
-write_excel_csv <- function(x, path, na = "NA", append = FALSE, col_names = !append) {
+write_excel_csv <- function(x, path, delim = ",", na = "NA", append = FALSE, col_names = !append) {
   stopifnot(is.data.frame(x))
 
-  x_out <- lapply(x, output_column)
-  stream_delim(x_out, path, ",", col_names = col_names, append = append,
+  x_formated <- if(delim == ";") {
+    x %>% dplyr::mutate_if(is.numeric, funs(format(., decimal.mark = ",")))
+  } else {
+    x
+  }
+  x_out <- lapply(x_formated, output_column)
+  stream_delim(x_out, path, delim, col_names = col_names, append = append,
     na = na, bom = TRUE)
 
   invisible(x)
+}
+
+#' @rdname write_delim
+#' @export
+write_excel_csv2 <- function(x, path,  delim = ";", na = "NA", append = FALSE, col_names = !append) {
+  write_excel_csv(x, path, delim, na, append, col_names)
 }
 
 #' @rdname write_delim
