5cc4325e02a2c46db3c7f74ac8ce3d64315ba0e2
Duncan Garmonsway <nacnudus@gmail.com>
2017-12-18 10:46:34 +0000

    Implement melt_csv

--- R/melt.R
+++ R/melt.R
@@ -22,6 +22,16 @@ melt_delim <- function(file, delim, quote = '"',
   melt_delimited(ds, tokenizer, locale, n_max)
 }
 
+#' @rdname melt_delim
+#' @export
+melt_csv <- function(file, locale = default_locale(), na = c("", "NA"),
+                     quoted_na = TRUE, quote = "\"", comment = "",
+                     trim_ws = TRUE, skip = 0, n_max = Inf) {
+  tokenizer <- tokenizer_csv(na = na, quoted_na = quoted_na, quote = quote,
+    comment = comment, trim_ws = trim_ws)
+  melt_delimited(file, tokenizer, locale = locale, skip = skip,
+                 comment = comment, n_max = n_max)
+}
 melt_tokens <- function(data, tokenizer, locale_, n_max) {
   if (n_max == Inf) {
     n_max <- -1
