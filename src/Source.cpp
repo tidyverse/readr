b00cc65953b783d76a182d67d59b86a884872dd2
Hiroaki Yutani <yutani.ini@gmail.com>
2018-05-01 19:49:42 +0900

    use Rf_translateChar()

--- src/Source.cpp
+++ src/Source.cpp
@@ -18,8 +18,8 @@ SourcePtr Source::create(List spec) {
     return SourcePtr(
         new SourceString(as<CharacterVector>(spec[0]), skip, comment));
   } else if (subclass == "source_file") {
-    std::string path(as<CharacterVector>(spec[0])[0]);
-    return SourcePtr(new SourceFile(path, skip, comment));
+    SEXP path(as<CharacterVector>(spec[0])[0]);
+    return SourcePtr(new SourceFile(Rf_translateChar(path), skip, comment));
   }
 
   Rcpp::stop("Unknown source type");
