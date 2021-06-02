b1d0a3010cc46bac7bc6d502b5717f43281f3916
Jim Hester <james.f.hester@gmail.com>
2021-06-02 08:18:37 -0400

    Ensure we can open non-ASCII filenames that are not in the system locale
    Fixes #1214

--- src/SourceFile.h
+++ src/SourceFile.h
@@ -8,6 +8,7 @@
 
 class SourceFile : public Source {
   mio::mmap_source source_;
+  FILE* fd_;
 
   const char* begin_;
   const char* end_;
@@ -21,7 +22,10 @@ public:
       bool skipQuotes = true) {
 
     std::error_code error;
-    source_ = make_mmap_source(path.c_str(), error);
+
+    fd_ = unicode_fopen(path.c_str(), "rb");
+
+    source_ = mio::make_mmap_source(fileno(fd_), error);
 
     if (error) {
       cpp11::stop("Cannot read file %s: %s", error.message().c_str());
@@ -36,6 +40,7 @@ public:
     // Skip lines, if needed
     begin_ = skipLines(begin_, end_, skip, skipEmptyRows, comment, skipQuotes);
   }
+  ~SourceFile() { std::fclose(fd_); }
 
   const char* begin() { return begin_; }
 
