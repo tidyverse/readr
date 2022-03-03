0ecd5599bfe59e586dd7e22648eebd3058f078d2
shelby <scbearrows@gmail.com>
2022-03-03 11:05:46 -0700

    add lifecycle imports

--- R/readr-package.R
+++ R/readr-package.R
@@ -3,6 +3,10 @@
 
 ## usethis namespace: start
 #' @importFrom hms hms
+#' @importFrom lifecycle deprecate_soft
+#' @importFrom lifecycle deprecate_warn
+#' @importFrom lifecycle deprecated
+#' @importFrom lifecycle is_present
 #' @importFrom R6 R6Class
 ## usethis namespace: end
 NULL
