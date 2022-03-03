60abfe0580c5f59f5879259e53de9ba8d37cc474
Shelby Bearrows <39840820+sbearrows@users.noreply.github.com>
2022-03-03 14:28:27 -0700

    Update package doc (#1379)
    * `usethis::use_package_doc()`
    
    * `usethis::use_import_from()`
    
    * add lifecycle imports
    
    * build docs
    
    * remove old doc

---
+++ R/readr-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom hms hms
+#' @importFrom lifecycle deprecate_soft
+#' @importFrom lifecycle deprecate_warn
+#' @importFrom lifecycle deprecated
+#' @importFrom lifecycle is_present
+#' @importFrom R6 R6Class
+## usethis namespace: end
+NULL
--- R/readr.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-#' @importFrom hms hms
-#' @importFrom R6 R6Class
-"_PACKAGE"
-
-## usethis namespace: start
-#' @importFrom lifecycle deprecated is_present deprecate_warn deprecate_soft
-## usethis namespace: end
-NULL
