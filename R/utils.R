d0fdb8de1179bea7c4eaf25851c168b363184b54
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-09-01 06:22:27 -0700

    Correct and improve docs for should_read_lazy()
    Closes #1400

--- R/utils.R
+++ R/utils.R
@@ -55,10 +55,26 @@ should_show_types <- function() {
   }
 }
 
-#' Determine whether reading should be done lazily
+#' Determine whether to read a file lazily
 #'
-#' Reading in readr is done lazily unless
-#' - `options(readr.read_lazy = FALSE)`
+#' @description
+#' This function consults the option `readr.read_lazy` to figure out whether to
+#' do lazy reading or not. If the option is unset, the default is `FALSE`,
+#' meaning readr will read files eagerly, not lazily. If you want to use this
+#' option to express a preference for lazy reading, do this:
+#'
+#' ```
+#' options(readr.read_lazy = TRUE)
+#' ```
+#'
+#' Typically, one would use the option to control lazy reading at the session,
+#' file, or user level. The `lazy` argument of functions like [read_csv()] can
+#' be used to control laziness in an individual call.
+#'
+#' @seealso The blog post ["Eager vs lazy reading in readr
+#'   2.1.0"](https://www.tidyverse.org/blog/2021/11/readr-2-1-0-lazy/) explains
+#'   the benefits (and downsides) of lazy reading.
+#'
 #' @export
 should_read_lazy <- function() {
   identical(getOption("readr.read_lazy", FALSE), TRUE)
