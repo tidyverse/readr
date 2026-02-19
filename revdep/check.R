c4b77876ba52c11487653118b25582f8b7905cc1
Jenny Bryan <jenny.f.bryan@gmail.com>
2026-02-18 21:33:20 -0800

    Clean out very old revdep stuff

--- revdep/check.R
+++
@@ -1,9 +0,0 @@
-library("devtools")
-
-# Don't forget to email javier@rstudio.com and ask him to check the IDE
-
-revdep_check()
-revdep_check_save_summary()
-revdep_check_print_problems()
-
-#revdep_email(date = "July 29", draft = FALSE)
